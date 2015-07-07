07c67947bd4696bde2ac642a189cbac613ca6c1e
Rabea Gleissner <rabeagleissner@gmail.com>
2015-07-07 07:24:52 +0100

    cell with only one neighbour dies

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -10,8 +10,13 @@ describe World do
   end
 
   describe 'alive cell' do
-    it 'kills one living cell with no neighbours' do
+    it 'dies with no neighbours' do
       world.create([0,0])
+      expect(world.still_alive?([0,0])).to eq false
+    end
+
+    it 'dies with only one neighbour' do
+      world.create([0,0], [0,1])
       expect(world.still_alive?([0,0])).to eq false
     end
 
