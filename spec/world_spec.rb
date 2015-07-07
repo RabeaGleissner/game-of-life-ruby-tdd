4889d56431d8d42311d46754b47b5a7c6269b3d2
Rabea Gleissner <rabeagleissner@gmail.com>
2015-07-07 07:21:03 +0100

    refactored tests and code to be more useful for future test cases

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -5,13 +5,16 @@ describe World do
   let(:world){World.new()}
 
   it 'creates an empty world' do
-    expect(world.create()).to eq []
+    world.create()
+    expect(world.empty?).to eq true
   end
 
   describe 'alive cell' do
     it 'kills one living cell with no neighbours' do
-      expect(world.create([0,0])). to eq []
+      world.create([0,0])
+      expect(world.still_alive?([0,0])).to eq false
     end
+
   end
 
 
--- world.rb
+++ world.rb
@@ -1,7 +1,19 @@
 class World
 
   def create(*cell)
-    []
+    if cell
+      still_alive?(cell)
+    else
+      self.empty?
+    end
+  end
+
+  def still_alive?(cell)
+    false
+  end
+
+  def empty?
+    true
   end
 
 end
